50b52cac84b3e9c663a7ca4288e53763f5aff85c
ikejiri <ikejiri@home-te.net>
2026-05-07 16:43:54 +0900

    GA登録

--- resources/views/user/components/ga.blade.php
+++ resources/views/user/components/ga.blade.php
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VLRC6S05E5"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-C51G57DLWL"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-VLRC6S05E5');
+  gtag('config', 'G-C51G57DLWL');
 </script>
